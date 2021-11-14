dbaab8fb08ab90333d7e842256921ddf4762dcdf
skowronskid <72418134+skowronskid@users.noreply.github.com>
2021-11-14 22:33:01 +0100

    kolory bardziej rozróźnialne

--- hashrate_country_share.R
+++ hashrate_country_share.R
@@ -12,10 +12,11 @@ cshare <- cshare %>%
   mutate(country = forcats::fct_reorder(country,monthly_hashrate_.,.desc = TRUE))
 cshare$monthly_hashrate_. <- as.numeric(sub("%", "",cshare$monthly_hashrate_.,fixed=TRUE))/100
 
-
+colo <- c("#006400","#00008b", "#b03060","#ff0000","#ffff00","#deb887","#00ff00","#00ffff","#ff00ff","#6495ed")
 
 ggplot(cshare, aes(x=dateNew, y=monthly_hashrate_., fill=country)) + 
   geom_area(alpha=0.6 , size=1, colour="black") + 
+  scale_fill_manual(values = colo) +
   geom_vline(xintercept = as.Date("2021/06/01") ,color="black", linetype = "dashed",size = 1)+
   labs(title = "Udzial panstw w hash-rate BTC",  x = "Data", y = "Udzial w globalnym hash-rate (srednia miesieczna)")
 
